cb6e43407391d5101d8c27af97aef06f15b1ed19
mandoz <mandoz@gmail.com>
2016-02-24 15:43:46 +0100

    Fixed log file path

--- run.py
+++ run.py
@@ -7,13 +7,15 @@ from checkers.replication import ReplicationChecker
 from notifiers.slack import SlackNotifier
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='replication.log', level=logging.DEBUG)
-    logging.info('Checker started at: ' + datetime.datetime.now().strftime(
-        '%Y-%m-%d %H:%M:%S'))
     directory = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__)))
     config = yaml.load(
         (open(os.path.join(directory, 'config.yml'), 'r').read()))
+
+    logging.basicConfig(filename=os.path.join(directory, 'replication.log'),
+                        level=logging.DEBUG)
+    logging.info('Checker started at: ' + datetime.datetime.now().strftime(
+        '%Y-%m-%d %H:%M:%S'))
 
     notifier = SlackNotifier(webhook_url=config['webhook_url'])
     checker = ReplicationChecker(
